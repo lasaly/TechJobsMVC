a30da9e64c240f24a2e9b663d5dc762e74ad204a
Chris Bay <chris@chrisbay.net>
2017-05-21 09:44:46 -0500

    VS 2017 migration

--- src/TechJobs/Controllers/SearchController.cs
+++ src/TechJobs/Controllers/SearchController.cs
@@ -15,6 +15,26 @@ namespace TechJobs.Controllers
 
         // TODO #1 - Create a Results action method to process 
         // search request and display results
+     
+        public IActionResult Results(string searchType, string searchTerm)
+        {
+            if (searchType.Equals("All"))
+            {
+                List<Dictionary<string, string>> jobs = JobData.FindByValue(searchTerm);
+                ViewBag.columns = ListController.columnChoices;
+                ViewBag.title = "Search Results";
+                ViewBag.jobs = jobs;
+                return View("Index");
+            }
+            else
+            {
+                List<Dictionary<string, string>> jobs = JobData.FindByColumnAndValue(searchType, searchTerm);
+                ViewBag.columns = ListController.columnChoices;
+                ViewBag.title = "Search Results";
+                ViewBag.jobs = jobs;
+                return View("Index");
+            }
+        }
 
     }
 }
